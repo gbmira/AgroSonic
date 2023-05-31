abe17e6db0e36113e8a4705dd29592d7da5ce318
gbmira <rm95694@fiap.com.br>
2023-05-31 00:45:54 -0300

    adicionado api

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,28 @@
 import axios from 'axios';
 
+
+
+
 const client = axios.create({
-  baseURL: 'http://localhost:8080',
+
+  baseURL: 'http://10.0.2.2:8080',
+
 });
 
-export {client};  
+
+
+
+
+// Configurações do CORS
+
+client.defaults.headers.common['Access-Control-Allow-Origin'] = '*'; // Permite todas as origens
+
+client.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS'; // Métodos permitidos
+
+client.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'; // Headers permitidos
+
+
+
+
+
+export { client }; 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,172 +1,421 @@
 import { Text, View, Image, StyleSheet, TouchableOpacity, TouchableHighlight, TextInput } from 'react-native';
+
 import React, { useState, useEffect, useCallback } from 'react';
 
+import { Formik } from 'formik';
 
-export default function Login ({ navigation }) {
+import axios from 'axios';
 
-  const [user, setUser] = useState();
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
+import { client } from "../../Api/index";
 
-  handleLoginUser = () => {
-    auth.signInWithEmailAndPassword(email, senha).then((user) => {
-      setUser(user);
-      alert('Login efetuado com sucesso!');
-      saveUserEmailToStorage();
-    }).catch((error) => {
-      console.log(error);
-      if (error.code === 'auth/user-not-found') {
-        alert('Conta não registrada. Cadastre-se! Por favor.');
+
+
+
+
+export default function Login({ navigation }) {
+
+
+
+
+
+  const handleLogin = async (values) => {
+
+    try {
+
+
+
+
+      const response = await client.post(`/api/auth/signin`, JSON.stringify(values), {
+
+        headers: {
+
+          'Content-Type': 'application/json'
+        }
+
+      })
+
+      console.log(response)
+
+
+
+
+      if (response.status === 200) {
+
+        alert(response.data)
+
       } else {
-        alert('E-mail ou senha incorretos! Tente novamente, por favor.');
+
+        alert("Erro de login")
+
       }
-    });
-  };
 
-return(
-  
+    } catch (error) {
 
-<View style={styles.container}>
-      
-     <View style={styles.header}>
+      console.error(`Erro ao realizar o login ${error}`)
 
-      <Image style={styles.imageH}  source={require('../assets/images/LogoAgroSonic.png')} />
+    }
 
-    </View>
-
-      <Text style={styles.text1}>Entre com sua conta!</Text>
-
-      <TextInput style={styles.inputs}
-      placeholder="Usuário"
-      placeholderTextColor="#408241"
-      onChangeText={(email) => setEmail(email)}
-      />
-          
-      <TextInput style={styles.inputs}
-        secureTextEntry
-        placeholder=" Senha"
-        placeholderTextColor="#408241"
-        onChangeText={(senha) => setSenha(senha)}
-      />
-
-      <TouchableOpacity style={styles.button}  >
-          <Text style={styles.buttonText}>Entrar</Text>
-        </TouchableOpacity>
+  }
 
 
 
 
-  <View style={styles.footer}>
 
-    <Text style={styles.textStart} onPress={() => navigation.navigate('SignUp')}>Desejo <Text style={styles.textEnd}>criar uma conta</Text></Text> 
-    
-    </View>
-
-  </View> );}
+  return (
 
 
- const styles = StyleSheet.create({
 
-  container: {  
+
+
+    <View style={styles.container}>
+
+
+
+
+      <View style={styles.header}>
+
+
+
+
+        <Image style={styles.imageH} source={require('../assets/images/LogoAgroSonic.png')} />
+
+
+
+
+      </View>
+
+
+
+
+      <View id='inputs-formik'>
+
+        <Formik
+
+          initialValues={{ email: '', password: '' }}
+
+          onSubmit={handleLogin}
+
+        >
+
+          {({ handleChange, handleSubmit, values }) => ( // Fix typo here
+
+            <>
+
+              <Text style={styles.text1}>Entre com sua conta!</Text>
+
+
+
+
+              <TextInput
+
+                onChangeText={handleChange('email')} // Fix typo here
+
+                value={values.email}
+
+                style={styles.inputs}
+
+                placeholder="Usuário"
+
+                placeholderTextColor="#408241"
+
+              />
+
+
+
+
+              <TextInput
+
+                onChangeText={handleChange('password')} // Fix typo here
+
+                value={values.password}
+
+                style={styles.inputs}
+
+                placeholder="Senha"
+
+                placeholderTextColor="#408241"
+
+              />
+
+              <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+
+                <Text style={styles.buttonText}>Entrar</Text>
+
+              </TouchableOpacity>
+
+
+
+
+
+            </>
+
+          )}
+
+        </Formik>
+
+
+
+
+
+      </View>
+
+
+
+
+      <View style={styles.footer}>
+
+
+
+
+        <Text style={styles.textStart} onPress={() => navigation.navigate('SignUp')}>Desejo <Text style={styles.textEnd}>criar uma conta</Text></Text>
+
+
+
+
+      </View>
+
+
+
+
+    </View>);
+
+}
+
+
+
+
+
+const styles = StyleSheet.create({
+
+
+
+
+  container: {
+
     flex: 1,
+
     backgroundColor: '#A8EFAF',
+
     alignItems: 'center',
-    justifyContent: 'center' },
 
-    header: {
-      justifyContent: 'center',
-      marginBottom: 40
-    },
+    justifyContent: 'center'
 
-    imageH: {
-      width: 321,
-      height: 129,
-      shadowColor: '#000',
+  },
+
+
+
+
+  header: {
+
+    justifyContent: 'center',
+
+    marginBottom: 40
+
+  },
+
+
+
+
+  imageH: {
+
+    width: 321,
+
+    height: 129,
+
+    shadowColor: '#000',
+
     shadowOffset: {
+
       width: 0,
+
       height: 5,
+
     },
+
     shadowOpacity: 0.20,
-    shadowRadius: 4,
-   },
 
-    text1:{
-    textAlign:'center',
+    shadowRadius: 4,
+
+  },
+
+
+
+
+  text1: {
+
+    textAlign: 'center',
+
     color: '#408241',
+
     marginBottom: 30,
+
     fontWeight: "700",
+
     fontSize: 17,
+
     textShadowColor: 'rgba(0, 0, 0, 0.15)',
+
     textShadowOffset: {
+
       width: 0,
+
       height: 3,
-    },
-    textShadowRadius: 2, },
 
-    inputs:{
-      textAlign: 'left',
-      width: '70%',
-      height: 55,
-      color: '#408241',
-      backgroundColor: '#A8EFAF',
-      borderRadius: 46.85,
-      borderWidth: 3,
-      borderColor: '#408241',
-      fontWeight: "700",
-      fontSize: 12.866,
-      padding: 10,
-      marginBottom: 20,
-      shadowColor: '#000',
+    },
+
+    textShadowRadius: 2,
+
+  },
+
+
+
+
+  inputs: {
+
+    textAlign: 'center',
+
+    width: 300,
+
+    height: 55,
+
+    color: '#408241',
+
+    backgroundColor: '#A8EFAF',
+
+    borderRadius: 46.85,
+
+    borderWidth: 3,
+
+    borderColor: '#408241',
+
+    fontWeight: "700",
+
+    fontSize: 12.866,
+
+    marginBottom: 20,
+
+    shadowColor: '#000',
+
     shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    borderRadius: 20, },
 
-    button:{
-      justifyContent: 'center',
-      width: '30%',
-      height: 55,
-      marginBottom: 30,
-      marginTop: 15,
-      borderRadius: 46.85,
-      backgroundColor: '#408241',
-      shadowColor: '#000',
+      width: 0,
+
+      height: 4,
+
+    },
+
+    shadowOpacity: 0.25,
+
+    shadowRadius: 4,
+
+    borderRadius: 20,
+
+  },
+
+
+
+
+  button: {
+
+    justifyContent: 'center',
+
+    width: '100%',
+
+    height: 55,
+
+    marginBottom: 30,
+
+    marginTop: 15,
+
+    borderRadius: 46.85,
+
+    backgroundColor: '#408241',
+
+    shadowColor: '#000',
+
     shadowOffset: {
+
       width: 0,
+
       height: 4,
+
     },
+
     shadowOpacity: 0.25,
+
     shadowRadius: 4,
-    borderRadius: 20, },
 
-    buttonText: {
-      textAlign: 'center',
-      color: '#ffffff',
-      fontWeight: "700",
-      fontSize: 16.866 },
+    borderRadius: 20,
 
-      footer: {
-        alignItems: 'center',
-        justifyContent: 'center', },
-      
-        textStart: {
-          textAlign:'center',
-          color: '#408241',
-          fontWeight: "700",
-          fontSize: 13.866,
-          textShadowColor: 'rgba(0, 0, 0, 0.15)',
-          textShadowOffset: {
-            width: 0,
-            height: 3,
-          },
-          textShadowRadius: 2, },
+  },
 
-          textEnd: {
-            textAlign:'center',
-            color: '#ffffff',
-            fontWeight: "700", },
-    
-  });
+
+
+
+  buttonText: {
+
+    textAlign: 'center',
+
+    color: '#ffffff',
+
+    fontWeight: "700",
+
+    fontSize: 16.866
+
+  },
+
+
+
+
+  footer: {
+
+    alignItems: 'center',
+
+    justifyContent: 'center',
+
+  },
+
+
+
+
+  textStart: {
+
+    textAlign: 'center',
+
+    color: '#408241',
+
+    fontWeight: "700",
+
+    fontSize: 13.866,
+
+    textShadowColor: 'rgba(0, 0, 0, 0.15)',
+
+    textShadowOffset: {
+
+      width: 0,
+
+      height: 3,
+
+    },
+
+    textShadowRadius: 2,
+
+  },
+
+
+
+
+  textEnd: {
+
+    textAlign: 'center',
+
+    color: '#ffffff',
+
+    fontWeight: "700",
+
+  },
+
+
+
+
+});
